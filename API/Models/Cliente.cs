7d4cba75fdb706cd7c2625bee5769ec7acd0a4b4
Gabriel-Muerte <rochalucasvb@gmail.com>
2026-03-30 17:06:42 -0300

    clientes

---
+++ API/Models/Cliente.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Models;
+
+public class Cliente
+{
+    public int Id { get; set; }
+    
+    
+
+    [Required(ErrorMessage = "O nome é obrigatório.")]
+    [StringLength(80, MinimumLength = 5, ErrorMessage = "O nome deve ter entre 5 e 80 caracteres.")]
+    public string Nome { get; set; }
+
+    [Required(ErrorMessage = "O Email é obrigatório.")]
+    [EmailAddress(ErrorMessage = "O Email deve ser válido.")]
+    public string Email { get; set; }
+
+    
+
+}   
--- API/Program.cs
+++ API/Program.cs
@@ -12,6 +12,13 @@ var categorias = new List<Categoria>
     new Categoria { Id = 2, Nome = "Vestuário", Descricao = "Roupas de marca" }
 };
 
+var clientes = new List<Cliente>
+{
+    new Cliente { Id = 1, Nome = "João Silva", Email = "joao.silva@example.com" },
+    new Cliente { Id = 2, Nome = "Maria Oliveira", Email = "maria.oliveira@example.com" }
+};
+
+
 app.MapGet("/", () => "Hello World!");
 
 #region Categoria
@@ -41,5 +48,73 @@ app.MapPut("/categorias/{id:int}", (int id, [FromBody] Categoria atualizarCatego
     return Results.NoContent();
 });
 #endregion
+
+#region Cliente - criar
+app.MapPost("/clientes/criar", (Cliente novoCliente) =>
+{
+    var validarResultado = new List<ValidationResult>();
+    var validarContexto = new ValidationContext(novoCliente);
+    
+    if (!Validator.TryValidateObject(novoCliente, validarContexto, validarResultado, true))
+    {
+        var error = validarResultado.FirstOrDefault(r => r.MemberNames.Contains("Email"))?.ErrorMessage 
+                    ?? validarResultado.FirstOrDefault()?.ErrorMessage;
+        
+        return Results.BadRequest(error);
+    }
+
+    novoCliente.Id = clientes.Count > 0 ? clientes.Max(c => c.Id) + 1 : 1;
+    clientes.Add(novoCliente);
+    return Results.Created($"/clientes/{novoCliente.Id}", novoCliente);
+});
+#endregion
+
+#region Cliente - Atualizar
+app.MapPut("/clientes/atualizar/{id:int}", (int id, [FromBody] Cliente atualizarCliente) =>
+{
+    var validarResultado = new List<ValidationResult>();
+    var validarContexto = new ValidationContext(atualizarCliente);
+    
+    if (!Validator.TryValidateObject(atualizarCliente, validarContexto, validarResultado, true))
+    {
+        var error = validarResultado.FirstOrDefault(r => r.MemberNames.Contains("Email"))?.ErrorMessage 
+                    ?? validarResultado.FirstOrDefault()?.ErrorMessage;
+        
+        return Results.BadRequest(error);
+    }
+
+    var cliente = clientes.FirstOrDefault(c => c.Id == id);
+    if (cliente == null)
+    {
+        return Results.NotFound();
+    }
+
+    cliente.Nome = atualizarCliente.Nome;
+    cliente.Email = atualizarCliente.Email;
+
+    return Results.NoContent();
+});
+#endregion
+
+#region Cliente - Listar
+app.MapGet("/clientes/listar", () => Results.Ok(clientes))
+    .WithName("GetClientes")
+    .WithTags("Clientes");
+#endregion
+
+#region Cliente - Eliminar
+app.MapDelete("/clientes/eliminar/{id:int}", (int id) =>
+{
+    var cliente = clientes.FirstOrDefault(c => c.Id == id);
+    if (cliente == null)
+    {
+        return Results.NotFound();
+    }
+
+    clientes.Remove(cliente);
+    return Results.NoContent();
+});
+#endregion
+
 
 app.Run();
